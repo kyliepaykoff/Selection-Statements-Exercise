1d28046f702b73ab3f547608389ef5c51783f5e7
Kylie Paykoff <kyliepaykoff@gmail.com>
2024-02-29 18:45:19 -0500

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("Guess my favorite number!");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < 22)
+            {
+                Console.WriteLine($"Sorry, too low");
+                
+       
+            }
+            else if (userInput > 22)
+            {
+                Console.WriteLine($"Sorry, too high");
+
+
+            }
+            else
+            {
+                Console.WriteLine($"Awesome! You guessed it!");
+
+            }
         }
     }
 }
