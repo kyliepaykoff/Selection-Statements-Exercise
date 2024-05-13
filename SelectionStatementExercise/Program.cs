b8653709af084dcf344859ce4e3c51c490b6d304
Kylie Paykoff <kyliepaykoff@gmail.com>
2024-05-13 16:02:11 -0400

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,40 +2,148 @@
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
-
-            var r = new Random();
-            var favNumber = r.Next(0, 1000);
-            int userInput;
-
-            do
+        static void 
+        
+            Main(string[] args)
             {
-                Console.WriteLine("Guess my favorite number! From 1-1000.");
 
-                userInput = int.Parse(Console.ReadLine());
-
-                if (userInput < favNumber)
+            PrintThousand();
+            Threes();
+            int a = 22;
+            int b = 22;
+            if (IsEqual(a, b))
+            {
+                Console.WriteLine($"{a} and {b} are equal.");
+            }
+            else
+            {
+                Console.WriteLine($"{a} and {b} are not equal.");
+            }
+            int c = 5;
+            if (IsEvenOdd(c))
+            {
+                Console.WriteLine($"The number is even.");
+            }
+            else
+            {
+                Console.WriteLine($"The number is odd.");
+            }
+            int d = -7;
+            if (IsPositive(d))
+            {
+                Console.WriteLine($"{d} is positive.");
+            }
+            else
+            {
+                Console.WriteLine($"{d} is negative.");
+            }
+            Console.WriteLine("Please enter your age:");
+            int age;
+            if (int.TryParse(Console.ReadLine(), out age))
+            {
+                if (CanVote(age))
                 {
-                    Console.WriteLine($"Sorry, too low");
-
-
-                }
-                else if (userInput > favNumber)
-                {
-                    Console.WriteLine($"Sorry, too high");
-
-
+                    Console.WriteLine("Congrats, you're old enough to vote!");
                 }
                 else
                 {
-                    Console.WriteLine($"Awesome! You guessed it!");
-
+                    Console.WriteLine("Sorry, you're not old enough to vote.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid age.");
+            }
+            Console.WriteLine($"Please enter a number from -10 to 10.");
+            int e;
+            if (int.TryParse(Console.ReadLine(), out e))
+            {
+                if (InRange(e))
+                {
+                    Console.WriteLine($"Good job!");
+                }
+                else
+                {
+                    Console.WriteLine($"Sorry, that is not in range.");
 
                 }
-                
-                
-            } while (userInput != favNumber);
+            }
+            Console.WriteLine($"Enter a number.");
+            int f;
+            if (int.TryParse(Console.ReadLine(), out f))
+            {
+                DisplayMultiplicationTable(f);
+            }
+            else
+            {
+                Console.WriteLine($"Please enter a valid number.");
+            }
+
+
+
+            } 
+
+        public static void PrintThousand()
+        {
+            for (int i = 1000; i >= -1000; i--)
+            {
+                Console.WriteLine(i);
+            }
+
         }
-    }
+
+        public static void Threes()
+        {
+            for (int k = 3; k <= 999; k += 3)
+            {
+                Console.WriteLine(k);
+            }
+        }
+
+        public static bool IsEqual(int a, int b)
+        {
+
+            return a == b;
+
+        }
+        public static bool IsEvenOdd(int c)
+
+        {
+
+
+            return c % 2 == 0;
+
+
+        }
+        public static bool IsPositive(int number)
+        {
+            return number > 0;
+        }
+        public static bool CanVote(int age)
+        {
+            const int votingAge = 18;
+            return age >= votingAge;
+        }
+        public static bool InRange(int e)
+        {
+            return e >= -10 && e <= 10;
+        }
+        public static void DisplayMultiplicationTable(int f)
+        {
+
+
+            Console.WriteLine($"Multiplication table of {f}");
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($"{f} x {i}; = {f * i}");
+            }
+        }
+
+
+
+
+
+
+    } 
+
 }
--- SelectionStatementExercise/obj/Debug/net6.0/SelectionStatementExercise.AssemblyInfo.cs
+++ SelectionStatementExercise/obj/Debug/net6.0/SelectionStatementExercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SelectionStatementExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d28046f702b73ab3f547608389ef5c51783f5e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed97c9d988b1e7749576c5bc4e6a30bf1929cde1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SelectionStatementExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SelectionStatementExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
